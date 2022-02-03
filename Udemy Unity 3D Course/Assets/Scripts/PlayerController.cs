87149776978d7f1a9f246e477cf36194d51c55dc
Ygonet1986 <igorcoutrimlacerda@gmail.com>
2022-02-03 18:34:57 -0300

    CharacterController Adicionado ao Player

--- Udemy Unity 3D Course/Assets/Scripts/PlayerController.cs
+++ Udemy Unity 3D Course/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     private Vector3 moveDirection;
+    public CharacterController charController;
+
+    private void Awake() {
+
+        charController = GetComponent<CharacterController>();
+
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +25,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical")); 
+        moveDirection = moveDirection * moveSpeed;
         
         if(Input.GetButtonDown("Jump")){
             moveDirection.y = jumpForce;
         }
 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime ; 
-       
+        //transform.position += moveDirection * moveSpeed * Time.deltaTime ; 
+        charController.Move( moveDirection * Time.deltaTime );
+
        
 
             
